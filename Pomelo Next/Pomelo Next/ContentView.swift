d081fca1e623fbf49d01755ae8c586a15030481c
Ramesh Basappareddy <ramesh.b@pomelofashion.com>
2021-03-01 14:34:37 +0700

    first commit

---
+++ Pomelo Next/Pomelo Next/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pomelo Next
+//
+//  Created by Ramesh-B on 1/3/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
